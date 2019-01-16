1cde5dac1b426d242481bba08ce2454e65e11b8f
daria marczak <marczak.ria@gmail.com>
2019-01-16 19:51:52 +0100

    Express has middleware for catching error

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ mongoose.Promise = global.Promise;
 app.use(bodyParser.json());
 app.use('/api', require('./routes/api'));
 
+app.use(function(err, req, res, next) {
+	res.status(422).send({ error: err.message });
+});
+
 app.listen(process.env.port || 4000, function() {
 	console.log('listening for requests');
 });
--- server/routes/api.js
+++ server/routes/api.js
@@ -7,21 +7,23 @@ router.get('/recipes', function(req, res) {
 	res.send({ type: 'GET' });
 });
 
-router.post('/recipes', function(req, res) {
-	Recipe.create(req.body).then(function(recipe) {
-		res.send({ recipe });
-	});
+router.post('/recipes', function(req, res, next) {
+	Recipe.create(req.body)
+		.then(function(recipe) {
+			res.send({ recipe });
+		})
+		.catch(next);
 });
 
-router.put('/recipes/:id', function(req, res) {
+router.put('/recipes/:id', function(req, res, next) {
 	res.send({ type: 'PUT' });
 });
 
-router.delete('/recipe/:id', function(req, res) {
+router.delete('/recipe/:id', function(req, res, next) {
 	res.send({ type: 'DELETE' });
 });
 
-router.get('/categories', function(req, res) {
+router.get('/categories', function(req, res, next) {
 	res.send({ type: 'GET' });
 });
 
