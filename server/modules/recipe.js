32c8e6a6c3343fad60c8f6300c89a4a380e07340
daria marczak <marczak.ria@gmail.com>
2019-01-16 19:19:37 +0100

    Added mongoose schema for recipe

---
+++ server/modules/recipe.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const RecipeSchema = new Schema({
+	title: {
+		type: String,
+		required: [true, 'Title of the recipe is required'],
+	},
+	category: {
+		type: String,
+		required: [true, 'Recipe category is required'],
+	},
+	imgUrl: {
+		type: String,
+		required: [true, 'Recipe needs an image'],
+	},
+	ingredients: {
+		type: Array,
+		required: [true, 'Ingredients are required'],
+	},
+	timeOfPreparation: {
+		type: Number,
+		required: true,
+	},
+	preparation: {
+		type: String,
+		required: true,
+	},
+	source: {
+		sourceName: {
+			type: String,
+			required: true,
+		},
+		sourceUrl: {
+			type: String,
+			required: true,
+		},
+	},
+});
