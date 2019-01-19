21471687e533986fa366bf09dfdc9e12eb4f43ab
daria marczak <marczak.ria@gmail.com>
2019-01-19 17:40:54 +0100

    Changed the recipe model a bit

--- server/modules/recipe.js
+++ server/modules/recipe.js
@@ -6,30 +6,40 @@ const RecipeSchema = new Schema({
 		type: String,
 		required: [true, 'Title of the recipe is required'],
 	},
-	category: String,
-	imgUrl: String,
+	category: {
+		type: String,
+		required: [true, 'Category is required'],
+	},
+	description: {
+		type: String,
+		required: [true, 'Description is required'],
+	},
+	imgUrl: {
+		type: String,
+		required: [true, 'Image is required'],
+	},
 	ingredients: {
 		type: Array,
 		required: [true, 'Ingredients are required'],
 	},
-	// timeOfPreparation: {
-	// 	type: Number,
-	// 	required: true,
-	// },
-	// preparation: {
-	// 	type: String,
-	// 	required: true,
-	// },
-	// source: {
-	// 	sourceName: {
-	// 		type: String,
-	// 		required: true,
-	// 	},
-	// 	sourceUrl: {
-	// 		type: String,
-	// 		required: true,
-	// 	},
-	// },
+	timeOfPreparation: {
+		type: Number,
+		required: true,
+	},
+	preparation: {
+		type: String,
+		required: true,
+	},
+	source: {
+		sourceName: {
+			type: String,
+			required: true,
+		},
+		sourceUrl: {
+			type: String,
+			required: true,
+		},
+	},
 });
 
 const Recipe = mongoose.model('recipe', RecipeSchema);
