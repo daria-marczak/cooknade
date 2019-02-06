b9809c919856696bda19de503f9d2fab3dd54f55
daria marczak <marczak.ria@gmail.com>
2019-02-06 20:32:29 +0100

    Image and title correctly positioned

--- client/src/components/Recipe.vue
+++ client/src/components/Recipe.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>{{recipe}}</div>
+  <div>
+    <img v-bind:src="recipe.imgUrl">
+    <v-container fluid grid-list-md pa-2 class="container">
+      <h2 v-html="recipe.title"/>
+      {{recipe}}
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -17,7 +23,25 @@ export default {
 </script>
 
 <style scoped>
-* {
-  background: pink;
+img {
+  width: 100%;
+  height: 50vh;
+  object-fit: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.container {
+  position: relative;
+  margin-top: 50vh;
+}
+
+h2 {
+  font-family: "Raleway", sans-serif;
+  font-weight: 600;
+  font-size: 18px;
 }
 </style>
