a6c3525cd78dd3396a4ed772226b0be8fa349b7d
daria marczak <marczak.ria@gmail.com>
2019-02-13 20:43:26 +0100

    Application is aware of the id

--- client/src/store/modules/auth.js
+++ client/src/store/modules/auth.js
@@ -3,21 +3,31 @@ import axios from 'axios';
 const state = {
 	isLoggedIn: true,
 	userId: '',
+	userName: '',
 };
 
 const url = '/auth/current_user';
 
 const actions = {
-	finishAuth: () => {
-		const response = axios.get(url).then(response => response);
-		console.log(response);
+	finishAuth: ({ commit }) => {
+		commit('authenticate');
 	},
 };
 
-const mutations = {};
+const mutations = {
+	authenticate: ({ commit }) => {
+		axios.get(url).then(response => {
+			state.userId = response.data._id;
+			state.userName = response.data.name;
+			console.log(response);
+		});
+	},
+};
 
 const getters = {
 	isLoggedIn: state => state.isLoggedIn,
+	userId: state => state.userId,
+	userName: state => state.userName,
 };
 
 export default { state, actions, mutations, getters };
